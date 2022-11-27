1cfe4e7310c5e94f3f859759f64680751d185629
MuzSh <muzamal2012@gmail.com>
2022-11-27 00:42:37 -0500

    update spacing

--- eyes.js
+++ eyes.js
@@ -1,11 +1,17 @@
 const balls = document.getElementsByClassName('ball');
+const eyes = document.getElementsByClassName('eye');
 document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 3; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
     balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+  for (let i = 0; i < 3; i++) {
+    eyes[i].style.left = x;
+    eyes[i].style.top = y;
+    eyes[i].transform = 'translate(-' + x + ',-' + y + ')';
   }
 };
