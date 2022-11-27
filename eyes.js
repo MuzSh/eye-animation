e44cbe6808fccbb7ce71d27632f93d64f3e15ee3
MuzSh <muzamal2012@gmail.com>
2022-11-27 01:47:36 -0500

    space

--- eyes.js
+++ eyes.js
@@ -4,14 +4,10 @@ document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
     balls[i].transform = 'translate(-' + x + ',-' + y + ')';
   }
-  for (let i = 0; i < 3; i++) {
-    eyes[i].style.left = x;
-    eyes[i].style.top = y;
-    eyes[i].transform = 'translate(-' + x + ',-' + y + ')';
-  }
+
 };
